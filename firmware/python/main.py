4ce704c91b87aafbfe35222057e95d024f07ea53
Enrico Cirignaco <e.cirignaco@gmail.com>
2020-12-14 00:32:28 +0100

    Starting display implemented.

--- firmware/python/main.py
+++ firmware/python/main.py
@@ -18,8 +18,11 @@ import handlers
 #############################################################################
 gpio.init()
 oled_display.init()
-oled_display.draw_entry(parameters.DEPTH_0_LABELS[0], parameters.MAIN_ENTRY_FONT_SIZE)
 parameters.update_dynamic_parameters()
+
+oled_display.draw_entry("Berrycase v0.2", 16)
+time.sleep(2)
+oled_display.draw_entry(parameters.DEPTH_0_LABELS[0], parameters.MAIN_ENTRY_FONT_SIZE)
 
 # Endless Loop
 #############################################################################
