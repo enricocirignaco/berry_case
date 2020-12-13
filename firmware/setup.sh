e884e14fcd175a114c3c9fb609fdf0e7398424fe
Enrico Cirignaco <e.cirignaco@gmail.com>
2020-12-14 00:52:44 +0100

    Changed location of installation script and readme. cronjob and poweroff features added to setup script.

--- firmware/setup.sh
+++ firmware/setup.sh
@@ -19,9 +19,21 @@ pip3 install adafruit-blinka
 pip3 install adafruit-circuitpython-ssd1306
 sudo apt-get install python3-pil
 
+# Add cronjob to run main script at startup
+#write out current crontab
+sudo crontab -u pi -l > mycron
+#echo new cron into cron file
+echo "@reboot /home/dev/documents/berry_case/firmware/python/autostart.sh" >> mycron
+#install new cron file
+sudo crontab -u pi mycron
+rm mycron
+
 # Changing i2c speed from 100kHz to 1MHz
 sudo cp /boot/config.txt /boot/config.txt.backup
 sudo sh -c 'echo "dtparam=i2c_baudrate=1000000" >> /boot/config.txt'
+
+# Add overlay to change GPIO25 to High after Shutdown
+sudo sh -c 'echo "dtoverlay=gpio-poweroff,gpiopin=25" >> /boot/config.txt'
 
 # Enable i2c
 echo "enable i2c and then reboot system. Afterwards you are ready to go"
